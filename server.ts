c89e26fb90233fd1a627f4b3f523371f1355dab9
Steven Tinsley <steven.tinsley81@gmail.com>
2022-03-03 18:56:16 +0000

    The get_pattern API endpoint has been refactored.  Also, some bugs related to promise rejections have been fixed.

--- server.ts
+++ server.ts
@@ -9,7 +9,7 @@ from './GameLogicTypes';
 // GameLogicTypes.getCellState and GameLogicTypes.setCellState.  See 20.cells for an example of
 // the string representation used.
 function deserialisePattern(pattern : string)
-                           : {gameBoard: BoardCell[], min : number, max : number} {
+                           : {gameBoard : BoardCell[], min : number, max : number} {
   const patternLines = pattern.split("\n");
   let maxLineLength = 0;
   const columnLength = patternLines.length;
@@ -47,11 +47,19 @@ function deserialisePattern(pattern : string)
   return gameBoardObject;
 }
 
+type PatternObject = {
+  boardArraySize : number,
+  liveCells: {
+    i : number,
+    j : number
+  }[]                  
+};
+
 // This function takes the object returned by deserialisePattern and returns a JSON string that
 // represents the game board pattern.  This two stage representation change ensures the pattern
 // will be auto centred around the coordinate system origin on the client side.
-function reserialisePattern(gameBoardObject : {gameBoard: BoardCell[], min : number, max : number})
-         : {boardArraySize : number, liveCells: {i: number, j: number}[]} {
+function reserialisePattern(gameBoardObject : {gameBoard : BoardCell[], min : number, max : number})
+                           : PatternObject {
   const liveCells = [];
   for (let i = gameBoardObject.min; i <= gameBoardObject.max; i++) {
     for (let j = gameBoardObject.min; j <= gameBoardObject.max; j++) {
@@ -67,54 +75,61 @@ function reserialisePattern(gameBoardObject : {gameBoard: BoardCell[], min : num
   return patternObject;
 }
 
-function getPatternQuery(connection, patternId : number) {
+type GetPatternResult = {
+  username : string,
+  comments : string,
+  patternObject: PatternObject
+}
+
+// This function queries the database by Pattern_id to retrieve a game board pattern and 
+// associated metadata.
+function getPatternQuery(connection, patternId : number)
+                        : Promise<{success : boolean, result : GetPatternResult | null}> {
   const sql = `SELECT Username, Comments, Pattern FROM pattern_catalogue
                WHERE Pattern_id = ${patternId}`;
-  const queryResolution = new Promise((resolve, reject) => {
-    connection.query(sql, (error, queryResult) => {
-      if (error) {
-        reject(error);
-      }
-      else if (queryResult.length === 0) {
-        reject(`Pattern_id ${patternId} could not be found in the database.`);
-      }
-      else {
-        const {Username, Comments, Pattern} = queryResult[0];
-        const gameBoardObject = deserialisePattern(Pattern);
-        const patternObject = reserialisePattern(gameBoardObject);
-        resolve({
-          username: Username,
-          comments: Comments,
-          patternObject: patternObject
-        });
-      }
+  const queryResolution =
+    new Promise<{success : boolean, result : GetPatternResult}>((resolve, reject) => {
+      connection.query(sql, (error, queryResult) => {
+        if (error) {
+          console.error(error);
+          reject({
+            success: false,
+            result: null
+          });
+        }
+        else if (queryResult.length === 0) {
+          console.error(`Pattern_id ${patternId} could not be found in the database.`);
+          reject({
+            success: false,
+            result: null
+          });
+        }
+        else {
+          const {Username, Comments, Pattern} = queryResult[0];
+          const gameBoardObject = deserialisePattern(Pattern);
+          const patternObject = reserialisePattern(gameBoardObject);
+          resolve({
+            success: true,
+            result: {
+              username: Username,
+              comments: Comments,
+              patternObject: patternObject
+            }
+          });
+        }
     });
   });
   return queryResolution;
 }
 
-function openDBConnection(connection) {
-  const connectResult = new Promise((resolve, reject) => {
-    connection.connect(error => {
-      if (error) {
-        reject(error);
-      }
-      else {
-        resolve("Successfully connected to the database.");
-      }
-    });
-  });
-  return connectResult;
-}
-
-async function getPattern(patternId : number) {
+function openDBConnection() : Promise<{success : boolean, connection : any}> {
   const fs = require("fs");
   let password = "";
   try {
     password = fs.readFileSync("./db_password.txt");
   }
   catch(error) {
-    console.log(`db_password.txt could not be opened.`);
+    console.error(`db_password.txt could not be opened.`);
     return null;
   }
 
@@ -126,36 +141,80 @@ async function getPattern(patternId : number) {
     database: "gol_pattern_catalogue"
   });
 
-  const connectResult = await openDBConnection(connection);
-  console.log(connectResult);
-  const result = await getPatternQuery(connection, patternId);
+  const connectPromise = new Promise<{success : boolean, connection : any}>((resolve, reject) => {
+    connection.connect(error => {
+      if (error) {
+        console.error(error);
+        reject({
+          success: false,
+          connection: null
+        });
+      }
+      else {
+        console.log("Successfully connected to the database.");
+        resolve({
+          success: true,
+          connection: connection
+        });
+      }
+    });
+  });
+  return connectPromise;
+}
 
-  connection.end(error => {
-    if (error) {
-      console.error(error);
+function closeDBConnection(connection) {
+  connection.end();
+  console.log("The database connection has been closed.");
+}
+
+// This function manages connecting to the database and attempting to retrieve the requested
+// game board pattern from it.
+async function getPattern(patternId : number)
+                         : Promise<{success : boolean, result : GetPatternResult | null}> {
+  const connectResult = await openDBConnection().catch();
+  const connection = connectResult.connection;
+  const queryResult = await getPatternQuery(connection, patternId).catch(() => {
+    closeDBConnection(connection);
+    return {success: false, result: null};
+  });
+  const result = new Promise<{success : boolean, result : GetPatternResult}>((resolve, reject) => {
+    if (connectResult.success === false || queryResult.success === false) {
+      reject({
+        success: false,
+        result: null
+      });
     }
     else {
-      console.log("The database connection has been closed.");
+      closeDBConnection(connection);
+      resolve({
+        success: true,
+        result: queryResult.result
+      });
     }
   });
   return result;
 }
 
-function main() {
+function main() : void {
   const express = require("express");
   const app = express();
   const PORT = process.env.PORT || 8080;
 
   app.get("/get_pattern", async function(request, response) {
+    console.log("");
     const patternId = parseInt(request.query.patternId);
-    if (patternId === NaN) {
-      console.error("get_pattern request received with invalid patternId query.");
+    if (patternId !== patternId) {
+      console.error("get_pattern request received with non - numeric patternId query field.");
       response.status(404).send("The requested patternId could not be found.").end();
     }
     else {
-      const result = await getPattern(patternId);
-      console.log(`main -> result: ${result}`);
-      response.status(200).send(result).end();
+      const result = await getPattern(patternId).catch(() => {
+        response.status(404).send("The requested patternId could not be found.").end();
+        return {success: false, result: null};
+      });
+      if (result.success) {
+        response.status(200).send(result.result).end();
+      }
     }
   });
 
